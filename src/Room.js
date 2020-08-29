bcc787c6d0a1d2d263438b63ed30bc4548dfbee4
Michael Pesek <pesekm@dupage.edu>
2020-08-29 12:02:35 -0500

    initial changes

--- src/Room.js
+++ src/Room.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Video from 'twilio-video';
 import Participant from './Participant';
+import db from '../firebase/firebase'
 
 const Room = ({ roomName, token, handleLogout }) => {
   const [room, setRoom] = useState(null);
@@ -9,6 +10,33 @@ const Room = ({ roomName, token, handleLogout }) => {
   useEffect(() => {
     const participantConnected = participant => {
       setParticipants(prevParticipants => [...prevParticipants, participant]);
+      console.log("participant is", participant)
+      console.log("room is", room)
+      if(participants.length === 0){
+        db.collection("rooms").doc(room).set({
+            Night: false,
+            checkMajority: false,
+            checkMedic: false,
+            checkSeer: false,
+            checkWerewolf: false,
+            dead: [],
+            gameStarted: false,
+            majorityReached: false,
+            medic: "",
+            medicChoice: "",
+            players: [],
+            seer: "",
+            seerChoice: "",
+            villagers: [],
+            villagersChoice: "",
+            votesVillagers: [],
+            votesWerewolves: [],
+            werewolves: [],
+            werewolvesChoice: ""
+        })
+      } // if
+      else{ // if the room is already created, we simply replace participants array with our new state
+        // db.collection("rooms").doc(room).update({participants: participants})
     };
 
     const participantDisconnected = participant => {
